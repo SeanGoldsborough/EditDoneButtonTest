b8bc4962a40061aaea7f2b9d436c6694e977daa7
Sean Goldsborough <smgoldsborough@hotmail.com>
2018-01-28 15:33:18 -0500

    Edit/Done Button

--- EditDoneButtonTest/ViewController.swift
+++ EditDoneButtonTest/ViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorBox: UIView!
+    
+    @IBAction func editButtonTapped(sender: AnyObject) {
+        let button =  sender as! UIBarButtonItem
+        if button.title! == "Edit" {
+            print("is editing now!")
+            button.title = "Done"
+            colorBox.backgroundColor = .blue
+        }
+        else{
+           print("is NOT editing now!")
+            button.title = "Edit"
+            colorBox.backgroundColor = .black
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         //navigationItem.rightBarButtonItem = editButtonItem
+        
+        //setEditing(editing: false, animated: true)
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +43,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    func setEditing (editing:Bool, animated:Bool)
+//    {
+//        super.setEditing(editing,animated:animated)
+//
+//        if(self.isEditing)
+//        {
+//            print("is editing now!")
+//            //self.editButtonItem.title = "Cancel"
+//            colorBox.backgroundColor = .blue
+//        } else
+//        {   print("is NOT editing now!")
+//            //self.editButtonItem.title = "Change"
+//            colorBox.backgroundColor = .black
+//        }
+//    }
 
 
 }
